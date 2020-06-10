49c9df8f41c59b40a2ac4d6fe45fc3a0b04f3d10
Roman Zubko <skcorpoma@gmail.com>
2020-06-10 10:40:52 +0300

    rewrite to class/add fetch to componentMount

--- src/components/VisibleTodoList.tsx
+++ src/components/VisibleTodoList.tsx
@@ -1,30 +1,49 @@
-import React, { FC } from 'react';
+import React, { Component } from 'react';
 import { connect, ConnectedProps } from 'react-redux';
 import { toggleTodo } from '../actions';
 import Todo from './Todo';
 import { RootState } from '../utils/interfaces';
 import { getVisibleTodos } from '../reducers';
+import { fetchTodos } from '../api';
 
 type Props = {
   filter: string;
 };
 
-const VisibleTodoList: FC<ConnectedProps<typeof connector>> = ({
-  todos,
-  onTodoClick,
-}) => {
-  return (
-    <ul>
-      {todos.map((todo) => (
-        <Todo key={todo.id} {...todo} onClick={() => onTodoClick(todo.id)} />
-      ))}
-    </ul>
-  );
-};
+class VisibleTodoList extends Component<ConnectedProps<typeof connector>> {
+  componentDidMount() {
+    fetchTodos(this.props.filter).then((todos) =>
+      console.log(this.props.filter, todos)
+    );
+  }
 
-const mapStateToProps = (state: RootState, ownProps: Props) => ({
-  todos: getVisibleTodos(state, ownProps.filter),
-});
+  componentDidUpdate(prevProps: ConnectedProps<typeof connector>) {
+    if (this.props.filter !== prevProps.filter) {
+      fetchTodos(this.props.filter).then((todos) =>
+        console.log(this.props.filter, todos)
+      );
+    }
+  }
+
+  render() {
+    const { todos, onTodoClick } = this.props;
+    return (
+      <ul>
+        {todos.map((todo) => (
+          <Todo key={todo.id} {...todo} onClick={() => onTodoClick(todo.id)} />
+        ))}
+      </ul>
+    );
+  }
+}
+
+const mapStateToProps = (state: RootState, ownProps: Props) => {
+  const filter = ownProps.filter;
+  return {
+    todos: getVisibleTodos(state, ownProps.filter),
+    filter,
+  };
+};
 
 const connector = connect(mapStateToProps, { onTodoClick: toggleTodo });
 export default connector(VisibleTodoList);
